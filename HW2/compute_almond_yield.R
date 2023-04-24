00183cf857c8915737c719baf4c658f2b93d26bb
n-mcmanus <nmcmanus@bren.ucsb.edu>
2023-04-24 11:52:05 -0700

    update

--- HW2/compute_almond_yield.R
+++ HW2/compute_almond_yield.R
@@ -1,12 +1,15 @@
-#' Almond yield anamoly model
+#' Almond yield anomaly model
 #'
-#' This function computes maximum, minimum, and mean yield anamoly for almonds
+#' This function computes maximum, minimum, and mean yield anomaly for almonds
 #' given a time series of minimum, maximum daily temperatures and precipitation
 #' 
 #' @param min_temp minimum temperature for february (degrees Celsius)
 #' @param precip precipitation during the month of january (mm)
-#' @return yield anamoly  (ton acre ^-1)
+#' @return yield anomaly  (ton acre ^-1)
 #' 
+
+
+
 
 almond_yield = function(min_temp, precip) {
   yield = -0.015*(min_temp) - 0.0046*(min_temp^2) - 0.07*(precip) + 0.0043*(precip^2) + 0.28
