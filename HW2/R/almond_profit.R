d745f607585ff76c07f5644264f6cd06371bd346
n-mcmanus <nmcmanus@bren.ucsb.edu>
2023-04-27 10:30:20 -0700

    profit model start

---
+++ HW2/R/almond_profit.R
@@ -0,0 +1,9 @@
+#' Almond profit model
+#'
+#' This function computes the profit of almond production based on the yield anomaly. 
+#' 
+#' @param min_temp minimum temperature for February (degrees Celsius)
+
+#' @return list of maximum, minimum, and average yield anomalies  (tons/acre)
+
+
