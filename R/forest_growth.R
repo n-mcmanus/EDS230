fb7ca06567d094a0f559e7307ec5a59aa6945686
n-mcmanus <nmcmanus@bren.ucsb.edu>
2023-05-23 14:32:09 -0700

    starting sobol stuff

--- R/forest_growth.R
+++ R/forest_growth.R
@@ -5,7 +5,7 @@
 #' @param time time since start
 #' @param params list of four parameter values (r, g, K, closure)
 #' @param C size of forest (measured in kg of carbon)
-#' @param K carrying capacity
+#' @param k carrying capacity
 #' @param r exponential growth rate (before canopy closure)
 #' @param g linear growth rate (after canopy closure
 #' @return forest size over time
@@ -14,7 +14,7 @@ forest_growth = function(time, C, params) {
   ## Rate of growth depending on canopy closure threshold
   dC = ifelse(C < params$closure, 
               params$r * C, 
-              params$g * (1-(C/params$K)))
+              params$g * (1-(C/params$k)))
   
   ## Rate of growth also depending on carrying capacity
   # dC = ifelse(C >= params$K, 0, dC)
